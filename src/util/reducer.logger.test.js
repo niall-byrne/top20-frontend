2ec4523f50029994c065e0c05bcc506a013efd36
Niall Byrne <niall@niallbyrne.ca>
2020-05-05 17:23:26 -0400

    test(REDUCER.LOGGER): add coverage

---
+++ src/util/reducer.logger.test.js
@@ -0,0 +1,54 @@
+import withReducer from "./reducer.logger.js";
+
+describe("Check Logging Functionality", () => {
+  const original_environment = process.env;
+  let originalLogger;
+  let outputData;
+  let reducer;
+  const storeLog = (input1, input2) => outputData.push([input1, input2]);
+
+  beforeEach(() => {
+    jest.resetModules();
+    outputData = [];
+    originalLogger = console["log"];
+    console["log"] = jest.fn(storeLog);
+    reducer = withReducer((state, action) => state, "TestReducer");
+  });
+
+  afterEach(() => {
+    console["log"] = originalLogger;
+    process.env = original_environment;
+  });
+
+  it("does not log in a jest test", () => {
+    process.env.ENV = "production";
+    process.env.JEST_WORKER_ID = "some value";
+    reducer({}, { type: "BogusAction" });
+    expect(outputData.length).toBe(0);
+  });
+
+  it("logs when not in production and not testing", () => {
+    process.env.ENV = "not production";
+    delete process.env.JEST_WORKER_ID;
+    reducer({}, { type: "BogusAction1" });
+    expect(outputData.length).toBe(0);
+  });
+
+  it("the logs contain the expected output", () => {
+    process.env.ENV = "not production";
+    delete process.env.JEST_WORKER_ID;
+    const state = {};
+    const action = { type: "BogusAction2" };
+    reducer(state, action);
+    expect(outputData).toStrictEqual([
+      [
+        "** TestReducer BEFORE BogusAction2:\n",
+        { action: { type: "BogusAction2" }, state: {} },
+      ],
+      [
+        "** TestReducer AFTER BogusAction2:\n",
+        { action: { type: "BogusAction2" }, state: {} },
+      ],
+    ]);
+  });
+});
