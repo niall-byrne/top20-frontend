a73d9dcefab29b1ed8abaa66f7773e566d242937
Niall Byrne <niall@niallbyrne.ca>
2020-05-05 17:23:42 -0400

    test(BILLBOARD): add coverage

---
+++ src/components/billboard/billboard.component.test.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import WithBillboard from "./billboard.component";
+
+const TestHook = () => <div>TestComponent</div>;
+
+describe("Check Spinner Rendering", () => {
+  let utils;
+  beforeEach(() => {
+    const TestHookWithBillboard = WithBillboard(TestHook);
+    utils = render(<TestHookWithBillboard />);
+  });
+
+  afterEach(cleanup);
+
+  it("renders encapsulated in the billboard", () => {
+    expect(utils.getByTestId("billboard1")).toBeTruthy();
+    expect(utils.getByTestId("billboard2")).toBeTruthy();
+    expect(utils.queryByText("TestComponent")).toBeTruthy();
+  });
+});
