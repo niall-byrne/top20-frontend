6f68c848f251444c8ac07405aa7e8b006d780163
Niall Byrne <niall@niallbyrne.ca>
2020-05-17 12:57:40 -0400

    test(INDEX): mock out the service worker registration and ensure it's called

--- src/tests/index.test.jsx
+++ src/tests/index.test.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { I18nextProvider } from "react-i18next";
-
 import App from "../App.js";
+import * as serviceWorker from "../serviceWorker";
 
+jest.mock("../serviceWorker", () => ({
+  __esModule: true,
+  register: jest.fn(),
+}));
 jest.mock("react-dom", () => ({ render: jest.fn() }));
 jest.mock("react-i18next");
 jest.mock("../App.js");
 
 App.mockImplementation(() => <div>MockApp</div>);
 I18nextProvider.mockImplementation(({ children }) => children);
+serviceWorker.register.mockImplementation(() => {
+  register: jest.fn();
+});
 
 describe("Check Main Rendering", () => {
   beforeEach(() => {
     App.mockClear();
     I18nextProvider.mockClear();
+    serviceWorker.register.mockClear();
   });
 
-  it("should call render on the index object as expected", () => {
+  it("should call render on the index object, and register the service worker as expected", () => {
     const div = document.createElement("div");
     div.id = "root";
     document.body.appendChild(div);
     const { Index } = require("../index.js");
     expect(ReactDOM.render).toHaveBeenCalledTimes(1);
     expect(ReactDOM.render).toHaveBeenCalledWith(<Index />, div);
+    expect(serviceWorker.register).toHaveBeenCalledTimes(1);
   });
 
   it("should render the main application components without crashing", () => {
